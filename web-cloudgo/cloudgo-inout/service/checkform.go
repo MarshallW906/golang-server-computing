f64d9efaeca4def9a8a2415e4ec0fdca6abee6b6
MarshallW906 <MaticWang@hotmail.com>
2017-11-22 13:06:36 +0800

    task 3 form ok [clean stdout]

--- web-cloudgo/cloudgo-inout/service/checkform.go
+++ web-cloudgo/cloudgo-inout/service/checkform.go
@@ -1,22 +1,20 @@
 package service
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func checkform(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "POST" {
 		r.ParseForm()
 		username := template.HTMLEscapeString(r.Form.Get("username"))
 		password := template.HTMLEscapeString(r.Form.Get("password"))
 		token := template.HTMLEscapeString(r.Form.Get("token"))
 		if token != "" {
-			//验证token的合法性
+			// validate the token
 		} else {
-			//不存在token报错
+			// if token doesn't exist
 		}
 		t := template.Must(template.New("checkform.html").ParseFiles("templates/checkform.html"))
 		err := t.Execute(w, struct {
